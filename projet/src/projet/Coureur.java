e996a7d5f3cadb04970ed69faab86ece7a8b2721
Etienne Chognard <chognard.etienne@gmail.com>
2017-06-08 10:50:29 +0200

    coureur

--- projet/src/projet/Coureur.java
+++ projet/src/projet/Coureur.java
@@ -2,34 +2,39 @@ package projet;
 
 import jade.core.AID;
 import jade.core.Agent;
-import jade.core.behaviours.Behaviour;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.core.behaviours.OneShotBehaviour;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 
-public class Coureur extends Agent {
+public class Coureur extends Agent{
 	int energie;
 	int fatigue;
 	int hydratation;
 	int stockEau;
 	int stockNourriture;
-	int puissanceMax;
+	//int vitesseMax;
 	char type;
-	int vitesseNormale;
-	int km = 0;
+	int vitesse;
+	int km;
 	public boolean leader = false;
 	String man;
-	int lieu;
-
-	public Coureur(String man) {
-		this.man=man;
-		int r = (Math.random() < 0.5) ? 0 : 1;
-		if (r == 0)
-			type = 's';
-		if (r == 1)
-			type = 'g';
-		this.lieu = 0;
+	
+	public Coureur(){
+		int r = (Math.random()<0.5)?0:1;
+		if(r==0)
+			type='s';
+		else if(r==1)
+			type='g';
+		km = 0;
+		energie = 50;
+		fatigue = 0;
+		hydratation = 50;
+		stockEau = 10;
+		stockNourriture = 10;
+		//vitesseMax = 15;
+		vitesse = 50;
+		
 	}
 	
 	protected void setup() {
@@ -39,19 +44,18 @@ public class Coureur extends Agent {
 		addBehaviour(new getNextSupplies());
 	}
 	
-	private class subscribeBehaviour extends OneShotBehaviour {
+	private class subscribeBehaviour extends OneShotBehaviour{
 		@Override
 		public void action() {
-			ACLMessage message = new ACLMessage(ACLMessage.SUBSCRIBE);
-			message.addReceiver(new AID("WORLD", AID.ISLOCALNAME));
-			send(message);
+			ACLMessage messageRace = new ACLMessage(ACLMessage.SUBSCRIBE);
+			messageRace.addReceiver(new AID("WORLD",AID.ISLOCALNAME));
+			send(messageRace);
 			ACLMessage messageManager = new ACLMessage(ACLMessage.SUBSCRIBE);
 			messageManager.addReceiver(new AID(man,AID.ISLOCALNAME));
 			send(messageManager);
 		}
 	}
 	
-
 	private class receiveInfo extends CyclicBehaviour {
 
 		@Override
@@ -86,63 +90,77 @@ public class Coureur extends Agent {
 			send(message);
 		}
 	}
-
-		private class getPente extends OneShotBehaviour {
-			@Override
-			public void action() {
-				ACLMessage message = new ACLMessage(ACLMessage.REQUEST);
-				message.addReceiver(new AID("WORLD", AID.ISLOCALNAME));
-				message.setContent("p" + km);
-				message.addReplyTo(getAID());
-				send(message);
-			}
-
-		}
-
-	private class getConsigne extends CyclicBehaviour {
+	
+	private class getPente extends OneShotBehaviour {
 		@Override
 		public void action() {
+			ACLMessage message = new ACLMessage(ACLMessage.REQUEST);
+			message.addReceiver(new AID("WORLD", AID.ISLOCALNAME));
+			message.setContent("p" + km);
+			message.addReplyTo(getAID());
+			send(message);
+		}
+
+	}
+
+	private class getConsigne extends CyclicBehaviour{
+		@Override
+		public void action(){
 			MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.INFORM);
 			ACLMessage message = receive(mt);
-			if (message != null) {
+			if(message != null){
 				String s = message.getContent();
-
+				
 			} else {
 				block();
 			}
 		}
 	}
-
-	public void boire() {
-		if (hydratation < 10 && stockEau > 0) {
-			hydratation += 5;
+	
+	public void boire(){
+		if(hydratation<10 && stockEau>0){
+			hydratation+=5;
 			stockEau--;
 		}
 	}
-
-	public void manger() {
-		if (energie < 10 && stockNourriture > 0) {
-			energie += 5;
+	
+	public void manger(){
+		if(energie<10 && stockNourriture>0){
+			energie+=5;
 			stockNourriture--;
 		}
 	}
-
-	public void repos() {
-		if (fatigue > 20) {
-			vitesseNormale -= 2;
+	
+	public void repos(){
+		if(fatigue>20){
+			vitesse-=2;
 			fatigue--;
 		}
 	}
-
-	public void majVitesseP(int pente) {
-		if (type == 's')
-			vitesseNormale += pente;
-		if (type == 'g')
-			vitesseNormale += pente;
+	
+	public void avancer(){
+		km+=km+(vitesse/3.6);
 	}
-
-	public void majVitesseC(int consigne) {
-		vitesseNormale += consigne;
+	
+	public void majVitesseP(int pente){
+		if(type=='s')
+			vitesse+=(pente*-0.01)*vitesse;
+		if(type=='g')
+			vitesse+=(pente*-0.005)*vitesse;
 	}
-
+	
+	public void majVitesseC(int consigne){
+			vitesse+=consigne;
+	}
+	
+	public void ravitailler(){
+		stockEau+=10;
+		stockNourriture+=10;
+	}
+	
+	public void ravitaillerVoiture(){
+		stockEau+=10;
+		stockNourriture+=10;
+	}
+	
 }
